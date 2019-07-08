ad01d11fd8a778f093e01ccbdefcb50e53b03c53
grinshpon <grinshpondaniel@protonmail.com>
2019-07-08 09:52:08 -0400

    remove commented out blocks

--- src/main.rs
+++ src/main.rs
@@ -70,29 +70,10 @@ fn draw_entities(canvas: &mut Canvas<Window>, entities: &HashMap<&str, Entity>, 
     canvas.copy(&textures[entity.img],Some(entity.src),Some(entity.dst()))
       .expect("Error rendering textures");
   }
-/*  for entity in entities.values() {
-    canvas.copy(&textures[entity.img],Some(entity.src),Some(entity.dst()))
-      .expect("Error loading texture");
-  }*/
 }
 
 fn run(canvas: &mut Canvas<Window>, event_pump: &mut EventPump) {
-    let texture_creator = canvas.texture_creator();
-/*
-  let mut entity_orb = Entity {
-    img: texture!(texture_creator,Path::new("assets/globe.png")),
-    z: 10,
-    src: Rect::new(0,0,64,64),
-    dst: Rect::new(10,0,512,512)
-  };
-  let mut entity_hp = Entity {
-    img: texture!(texture_creator,Path::new("assets/globefill.png")),
-    z: 9,
-    src: Rect::new(0,0,64,64),
-    dst: Rect::new(10,0,512,512)
-  };
-  let mut entities = vec![&entity_hp, &entity_orb];
-*/
+  let texture_creator = canvas.texture_creator();
   let mut textures = hashmap!
     [ "texture_orb" => texture!(texture_creator,Path::new("assets/globe.png"))
     , "texture_bar" => texture!(texture_creator,Path::new("assets/globefill.png"))
@@ -111,13 +92,7 @@ fn run(canvas: &mut Canvas<Window>, event_pump: &mut EventPump) {
       , Rect::new(10,0,512,512)
       )
     ];
-/*
-  let frame_orb = Rect::new(10,0,512,512);
-  // 0,32,64,32
-  let mut health = Rect::new(0,0,64,64);
-  // 10,256,512,256
-  let mut frame_health = Rect::new(10,0,512,512);
-*/
+
   let mut hp: u32 = 98;
   let mut fhp: f32 = 64.0 * ((hp as f32)/100.0);
 
@@ -128,21 +103,10 @@ fn run(canvas: &mut Canvas<Window>, event_pump: &mut EventPump) {
     if fhp > 31.0 { //min visibility at 4, 1 down on 96, fhp: 3.0 - 62.0
       hp -= 1;
       fhp = 64.0 * (hp as f32/100.0);
-      //fhp -= 1.0;
-/*      health.y = 64 - fhp as i32;
-      health.set_height(fhp as u32);
-      frame_health.y = 512 - (fhp as i32 * 8);
-      frame_health.set_height(fhp as u32 * 8);
-*/
       get!(entities,&"bar").src.y = 64 - fhp as i32;
       get!(entities,&"bar").src.set_height(fhp as u32);
       get!(entities,&"bar").setY(512 - (fhp as i32 * 8));
       get!(entities,&"bar").set_height(fhp as u32 * 8);
-      //println!("{:?}\t{:?}", entities[0].src,entities[0].dst);
-/*
-      entity_hp.src.y = 64 - fhp as i32;
-      println!("{:?}",entity_hp.src);
-*/
     }
     canvas.clear();
     for event in event_pump.poll_iter() {
@@ -155,8 +119,6 @@ fn run(canvas: &mut Canvas<Window>, event_pump: &mut EventPump) {
       }
     }
     // The rest of the game loop goes here...
-    //canvas.copy(&, Some(health), Some(frame_health)).expect("Error drawing texture");
-    //canvas.copy(&texture_orb, None, Some(frame_orb)).expect("Error drawing texture");
     draw_entities(canvas, &entities, &textures);
     canvas.present();
     std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
